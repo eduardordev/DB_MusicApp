0782731f8a80ad88bade2f1534d2fc844db7e491
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2021-03-23 12:49:36 -0600

    connection to bd, done

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const pg = require('pg')
-const connectionString = "postgres://postgres:456528@localhost/127.0.0.1:58133/nameOfDatabase/proyecto1"
-
-const client = new pg.Client(connectionString);
-
-client.connect();
-
