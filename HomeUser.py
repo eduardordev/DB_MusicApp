d5c511ce6e9f009acb77d6b88b0a46e6594132eb
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2021-05-11 02:44:01 -0600

    Login, Register & Home user done

--- HomeUser.py
+++ HomeUser.py
@@ -26,145 +26,106 @@ class Ui_HomeUser(object):
         MainWindow.resize(1000, 650)
         MainWindow.setMinimumSize(QtCore.QSize(1000, 650))
         MainWindow.setMaximumSize(QtCore.QSize(1000, 650))
-        MainWindow.setStyleSheet("background-color: rgb(72,35,60);")
+        MainWindow.setStyleSheet("background-color: #d1d1d5;")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.frame = QtWidgets.QFrame(self.centralwidget)
         self.frame.setGeometry(QtCore.QRect(50, 50, 900, 550))
         self.frame.setMinimumSize(QtCore.QSize(900, 550))
         self.frame.setMaximumSize(QtCore.QSize(900, 550))
-        self.frame.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-"border-radius: 12px;")
+        self.frame.setStyleSheet("background-color: #1c1d32;\n"
+"border-radius: 5px;")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
-        self.label = QtWidgets.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(130, 50, 205, 50))
-        self.label.setMinimumSize(QtCore.QSize(205, 50))
-        self.label.setMaximumSize(QtCore.QSize(205, 50))
-        self.label.setStyleSheet("font: 24pt \"Times\";\n"
-"color: rgb(10, 54, 157);\n"
-"")
-        self.label.setObjectName("label")
+
         self.label_2 = QtWidgets.QLabel(self.frame)
-        self.label_2.setGeometry(QtCore.QRect(20, 20, 100, 100))
-        self.label_2.setMinimumSize(QtCore.QSize(100, 100))
-        self.label_2.setMaximumSize(QtCore.QSize(100, 100))
+        self.label_2.setGeometry(QtCore.QRect(100, 20, 100, 100))
+        self.label_2.setMinimumSize(QtCore.QSize(225, 205))
+        self.label_2.setMaximumSize(QtCore.QSize(225, 205))
         self.label_2.setStyleSheet("")
         self.label_2.setText("")
-        self.label_2.setPixmap(QtGui.QPixmap("logo.png"))
+        self.label_2.setPixmap(QtGui.QPixmap("musics.PNG"))
         self.label_2.setScaledContents(True)
         self.label_2.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.label_2.setObjectName("label_2")
+
         self.tableWidget = QtWidgets.QTableWidget(self.frame)
-        self.tableWidget.setGeometry(QtCore.QRect(30, 230, 841, 281))
-        self.tableWidget.setStyleSheet("font: 13pt \"Times\";\n"
-"color: rgb(64, 55, 110);\n"
-"background-color: rgb(212, 228, 188);")
+        self.tableWidget.setGeometry(QtCore.QRect(450, 30, 400, 490))
+        self.tableWidget.setStyleSheet("font: 18pt \"Times\";\n"
+"color: #1b1c34;\n"
+"background-color: #EDFEFB")
         self.tableWidget.setObjectName("tableWidget")
-        #self.tableWidget.setColumnCount(5)
-        #self.tableWidget.setRowCount(6)
-        #item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setVerticalHeaderItem(0, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setVerticalHeaderItem(1, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setVerticalHeaderItem(2, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setVerticalHeaderItem(3, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setVerticalHeaderItem(4, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setVerticalHeaderItem(5, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setHorizontalHeaderItem(0, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setHorizontalHeaderItem(1, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setHorizontalHeaderItem(2, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setHorizontalHeaderItem(3, item)
-        # item = QtWidgets.QTableWidgetItem()
-        # self.tableWidget.setHorizontalHeaderItem(4, item)
+
+
         self.pushButton_Exit = QtWidgets.QPushButton(self.frame)
-        self.pushButton_Exit.setGeometry(QtCore.QRect(756, 50, 114, 32))
-        self.pushButton_Exit.setMinimumSize(QtCore.QSize(114, 32))
-        self.pushButton_Exit.setMaximumSize(QtCore.QSize(114, 32))
-        self.pushButton_Exit.setStyleSheet("background-color: rgb(10, 54, 157);\n"
-"font: 14pt \"Times\";\n"
+        self.pushButton_Exit.setGeometry(QtCore.QRect(20, 470, 114, 32))
+        self.pushButton_Exit.setMinimumSize(QtCore.QSize(410, 50))
+        self.pushButton_Exit.setMaximumSize(QtCore.QSize(410, 50))
+        self.pushButton_Exit.setStyleSheet("background-color: #0ca692;\n"
+"font: 24pt \"Times\";\n"
 "color: rgb(255, 255, 255);\n"
-"border-radius: 12px;")
+"border-radius: 5px;")
         self.pushButton_Exit.setObjectName("pushButton_Exit")
         self.pushButton_Exit.clicked.connect(QtCore.QCoreApplication.instance().quit)
         self.textEdit_UserBuscar = QtWidgets.QTextEdit(self.frame)
-        self.textEdit_UserBuscar.setGeometry(QtCore.QRect(565, 200, 231, 31))
-        self.textEdit_UserBuscar.setStyleSheet("background-color: rgb(150, 172, 183);\n"
-"font: 13pt \"Times\";\n"
-"color: rgb(255, 255, 255);\n"
-"border-radius: 12px;")
+        self.textEdit_UserBuscar.setGeometry(QtCore.QRect(20, 350, 231, 31))
+        self.textEdit_UserBuscar.setMinimumSize(QtCore.QSize(410, 50))
+        self.textEdit_UserBuscar.setMaximumSize(QtCore.QSize(410, 50))
+        self.textEdit_UserBuscar.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"font: 18pt \"Times\";\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 5px;")
         self.textEdit_UserBuscar.setObjectName("textEdit_UserBuscar")
-        self.label_4 = QtWidgets.QLabel(self.frame)
-        self.label_4.setGeometry(QtCore.QRect(534, 199, 31, 31))
-        self.label_4.setStyleSheet("")
-        self.label_4.setText("")
-        self.label_4.setPixmap(QtGui.QPixmap("finder.png"))
-        self.label_4.setScaledContents(True)
-        self.label_4.setObjectName("label_4")
-        self.label_8 = QtWidgets.QLabel(self.frame)
-        self.label_8.setGeometry(QtCore.QRect(30, 130, 210, 25))
-        self.label_8.setMinimumSize(QtCore.QSize(210, 25))
-        self.label_8.setMaximumSize(QtCore.QSize(210, 25))
-        self.label_8.setStyleSheet("font: 18pt \"Times\";\n"
-"color: rgb(10, 54, 157);\n"
-"")
-        self.label_8.setObjectName("label_8")
+
+
         self.comboBox_OpcionesBuscar = QtWidgets.QComboBox(self.frame)
-        self.comboBox_OpcionesBuscar.setGeometry(QtCore.QRect(30, 160, 220, 31))
-        self.comboBox_OpcionesBuscar.setMinimumSize(QtCore.QSize(220, 31))
-        self.comboBox_OpcionesBuscar.setMaximumSize(QtCore.QSize(181, 31))
-        self.comboBox_OpcionesBuscar.setStyleSheet("background-color: rgb(150, 172, 183);\n"
-"font: 13pt \"Times\";\n"
-"color: rgb(255, 255, 255);\n"
-"border-radius: 12px;")
+        self.comboBox_OpcionesBuscar.setGeometry(QtCore.QRect(20, 290, 220, 31))
+        self.comboBox_OpcionesBuscar.setMinimumSize(QtCore.QSize(410, 50))
+        self.comboBox_OpcionesBuscar.setMaximumSize(QtCore.QSize(410, 50))
+        self.comboBox_OpcionesBuscar.setStyleSheet("background-color: #d1d1d5;\n"
+"font: 14pt \"Times\";\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 5px;")
         self.comboBox_OpcionesBuscar.setObjectName("comboBox_OpcionesBuscar")
         self.comboBox_OpcionesBuscar.addItem("")
         self.comboBox_OpcionesBuscar.addItem("")
         self.comboBox_OpcionesBuscar.addItem("")
         self.comboBox_OpcionesBuscar.addItem("")
         self.comboBox_OpcionesBuscar.addItem("")
+
+
         self.pushButton_Buscar = QtWidgets.QPushButton(self.frame)
-        self.pushButton_Buscar.setGeometry(QtCore.QRect(796, 200, 75, 31))
-        self.pushButton_Buscar.setMinimumSize(QtCore.QSize(75, 31))
-        self.pushButton_Buscar.setMaximumSize(QtCore.QSize(75, 31))
-        self.pushButton_Buscar.setStyleSheet("background-color: rgb(10, 54, 157);\n"
-"font: 14pt \"Times\";\n"
+        self.pushButton_Buscar.setGeometry(QtCore.QRect(20, 410, 75, 31))
+        self.pushButton_Buscar.setMinimumSize(QtCore.QSize(410, 50))
+        self.pushButton_Buscar.setMaximumSize(QtCore.QSize(410, 50))
+        self.pushButton_Buscar.setStyleSheet("background-color: #0ca692;\n"
+"font: 24pt \"Times\";\n"
 "color: rgb(255, 255, 255);\n"
-"border-radius: 12px;")
+"border-radius: 5px;")
         self.pushButton_Buscar.setObjectName("pushButton_Buscar")
         self.pushButton_Buscar.clicked.connect(self.populateTable)
+
         self.pushButton_Tienda = QtWidgets.QPushButton(self.frame)
-        self.pushButton_Tienda.setGeometry(QtCore.QRect(630, 50, 114, 32))
-        self.pushButton_Tienda.setMinimumSize(QtCore.QSize(114, 32))
-        self.pushButton_Tienda.setMaximumSize(QtCore.QSize(114, 32))
-        self.pushButton_Tienda.setStyleSheet("background-color: rgb(10, 54, 157);\n"
-"font: 14pt \"Times\";\n"
+        self.pushButton_Tienda.setGeometry(QtCore.QRect(20, 220, 114, 32))
+        self.pushButton_Tienda.setMinimumSize(QtCore.QSize(200, 50))
+        self.pushButton_Tienda.setMaximumSize(QtCore.QSize(200, 50))
+        self.pushButton_Tienda.setStyleSheet("background-color: #0ca692;\n"
+"font: 16pt \"Times\";\n"
 "color: rgb(255, 255, 255);")
         self.pushButton_Tienda.setObjectName("pushButton_Tienda")
         self.pushButton_MiMusica = QtWidgets.QPushButton(self.frame)
-        self.pushButton_MiMusica.setGeometry(QtCore.QRect(504, 50, 114, 32))
-        self.pushButton_MiMusica.setMinimumSize(QtCore.QSize(114, 32))
-        self.pushButton_MiMusica.setMaximumSize(QtCore.QSize(114, 32))
-        self.pushButton_MiMusica.setStyleSheet("background-color: rgb(10, 54, 157);\n"
-"font: 14pt \"Times\";\n"
+        self.pushButton_MiMusica.setGeometry(QtCore.QRect(230, 220, 114, 32))
+        self.pushButton_MiMusica.setMinimumSize(QtCore.QSize(200, 50))
+        self.pushButton_MiMusica.setMaximumSize(QtCore.QSize(200, 50))
+        self.pushButton_MiMusica.setStyleSheet("background-color: #0ca692;\n"
+"font: 16pt \"Times\";\n"
 "color: rgb(255, 255, 255);")
         self.pushButton_MiMusica.setObjectName("pushButton_Mongo")
-        self.label_8.raise_()
-        self.label.raise_()
         self.label_2.raise_()
         self.tableWidget.raise_()
         self.pushButton_Exit.raise_()
         self.textEdit_UserBuscar.raise_()
-        self.label_4.raise_()
         self.comboBox_OpcionesBuscar.raise_()
         self.pushButton_Tienda.raise_()
         self.pushButton_Tienda.clicked.connect(self.openTienda)
@@ -178,36 +139,15 @@ class Ui_HomeUser(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Sonido Cloud "))
-        # item = self.tableWidget.verticalHeaderItem(0)
-        # item.setText(_translate("MainWindow", "aqui"))
-        # item = self.tableWidget.verticalHeaderItem(1)
-        # item.setText(_translate("MainWindow", "van"))
-        # item = self.tableWidget.verticalHeaderItem(2)
-        # item.setText(_translate("MainWindow", "New Row"))
-        # item = self.tableWidget.verticalHeaderItem(3)
-        # item.setText(_translate("MainWindow", "las"))
-        # item = self.tableWidget.verticalHeaderItem(4)
-        # item.setText(_translate("MainWindow", "filas"))
-        # item = self.tableWidget.verticalHeaderItem(5)
-        # item.setText(_translate("MainWindow", "proyecto"))
-        # item = self.tableWidget.horizontalHeaderItem(0)
-        # item.setText(_translate("MainWindow", "aqui"))
-        # item = self.tableWidget.horizontalHeaderItem(1)
-        # item.setText(_translate("MainWindow", "van"))
-        # item = self.tableWidget.horizontalHeaderItem(2)
-        # item.setText(_translate("MainWindow", "las"))
-        # item = self.tableWidget.horizontalHeaderItem(3)
-        # item.setText(_translate("MainWindow", "columnas"))
-        # item = self.tableWidget.horizontalHeaderItem(4)
-        # item.setText(_translate("MainWindow", "proyecto"))
+        
+
         self.pushButton_Exit.setText(_translate("MainWindow", "Salir"))
         self.textEdit_UserBuscar.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Times\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
-        self.label_8.setText(_translate("MainWindow", "Módulo de búsquedas"))
+        
         self.comboBox_OpcionesBuscar.setItemText(0, _translate("MainWindow", "¿Qué deseas buscar?"))
         self.comboBox_OpcionesBuscar.setItemText(1, _translate("MainWindow", "Artista"))
         self.comboBox_OpcionesBuscar.setItemText(2, _translate("MainWindow", "Género"))
